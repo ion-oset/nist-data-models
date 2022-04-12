004a09d8a65c00ba57776d441ff24707084ab4b7
Ion Y <ion@osetinstitute.org>
2022-04-12 10:28:24 -0700

    Enable snake-case field names in all NIST models. Use the alias transform to convert camel-case fields to snake-case.

--- src/electos/datamodels/nist/models/base.py
+++ src/electos/datamodels/nist/models/base.py
@@ -1,8 +1,46 @@
+import re
+
 import pydantic
+
+
+def _split_snake_case(text):
+    """Tokenize a string using underscores as delimiters."""
+    for match in re.finditer("(^_+|[A-Za-z][A-Za-z0-9]*)", text):
+        yield match.group(0)
+
+
+def _snake_to_camel(text):
+    """Convert text in 'snake_case' to 'CamelCase'."""
+    return "".join(part.capitalize() for part in _split_snake_case(text))
+
+
+class _NistModelConfig:
+
+    """Settings that apply to all NistModels and subtypes."""
+
+    # Replace leading '@' character in JSON Schema field names.
+
+    _AT_PREFIX = "_"
+
+    def _field_name_alias(field_name, at_prefix = _AT_PREFIX):
+        """Convert fieldnames:
+
+        - Assume snake case for fields, use camel case for serialized JSON.
+        - Set the prefix that maps to '@'-prefix schema keys (e.g. '@type').
+        """
+        if field_name.startswith(at_prefix):
+            n = len(at_prefix)
+            alias = f"@{field_name[n:]}"
+        else:
+            alias = _snake_to_camel(field_name)
+        return alias
+
+    # Convert field names.
+    alias_generator = _field_name_alias
 
 
 class NistModel(pydantic.BaseModel):
 
     """Common state for all classes that are models for the NIST JSON Schema."""
 
-    pass
+    Config = _NistModelConfig
